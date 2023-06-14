1e6c854b20d89cdec44f7c981a89f68de2a9fc40
Nayanda Haberty <hainayanda@outlook.com>
2023-06-14 16:33:02 +0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Retain",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Retain",
+            targets: ["Retain"]
+        )
+    ],
+    dependencies: [
+        // uncomment code below to test
+//        .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
+//        .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Retain",
+            dependencies: [],
+            path: "Retain/Classes"
+        ),
+        // uncomment code below to test
+//       .testTarget(
+//           name: "RetainTests",
+//           dependencies: [
+//               "Retain", "Quick", "Nimble"
+//           ],
+//           path: "Example/Tests",
+//           exclude: ["Info.plist"]
+//       )
+    ]
+)
